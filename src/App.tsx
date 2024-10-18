1f9460335235d576ed2ab87906fbd75d68fb7b18
Eckry <112786854+Eckry@users.noreply.github.com>
2024-10-17 20:29:26 -0600

    add initial movement to a dot

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,56 @@
+import { useEffect, useRef, useState } from "react";
 import "./App.css";
+import { Dot } from "./types.d";
+
+const initialDots: Dot[] = [{ x: 35, y: 15, vx: 1, vy: 1, radius: 10 }];
 
 function App() {
-  return <main>Hello!</main>;
+  const dots = useRef<Dot[]>(initialDots);
+  const ref = useRef<null | HTMLCanvasElement>(null);
+
+  useEffect(() => {
+    let requestId: number;
+    function animate() {
+      const canvas = ref.current;
+
+      if (!canvas) return;
+      canvas.width = 200;
+      canvas.height = 200;
+
+      const ctx = canvas.getContext("2d");
+
+      if (!ctx) return;
+
+      ctx.beginPath();
+      dots.current.forEach((dot) => {
+        ctx.arc(dot.x, dot.y, dot.radius, 0, 2 * Math.PI);
+      });
+      ctx.fill();
+
+      dots.current = dots.current.map((dot) => {
+        return { ...dot, x: dot.x + dot.vx, y: dot.y + dot.vy };
+      });
+
+      dots.current.forEach((dot) => {
+        if (dot.x - dot.radius < 0 || dot.x + dot.radius > canvas.width)
+          dot.vx *= -1;
+        if (dot.y - dot.radius < 0 || dot.y + dot.radius > canvas.height)
+          dot.vy *= -1;
+      });
+
+      requestId = requestAnimationFrame(animate);
+    }
+
+    animate();
+
+    return () => cancelAnimationFrame(requestId);
+  }, []);
+
+  return (
+    <main>
+      <canvas ref={ref ? ref : null}></canvas>
+    </main>
+  );
 }
 
 export default App;
---
+++ src/types.d.ts
@@ -0,0 +1,7 @@
+export interface Dot {
+  x: number;
+  y: number;
+  vx: number;
+  vy: number;
+  radius: number
+}
